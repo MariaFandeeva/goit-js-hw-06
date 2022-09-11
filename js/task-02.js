c706872ec5bc9aaa065ef4d65a547b074e5c714d
MariaFandeeva <mfandeeva@gmail.com>
2022-09-11 21:19:07 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsEl = document.querySelector("#ingredients");
+
+const listElements = ingredients.map((ingredient) => {
+  const listEl = document.createElement("li");
+  listEl.textContent = ingredient;
+  listEl.classList.add("item");
+  return listEl;
+});
+
+ingredientsEl.append(...listElements);
