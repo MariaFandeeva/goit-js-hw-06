bb34d43c208d7ec3b4aaa5e769d969057edf59fb
MariaFandeeva <mfandeeva@gmail.com>
2022-09-15 18:06:21 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+const onSubmitHandler = (event) => {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return aleret(`Please enter email and password`);
+  }
+  const dataEl = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(dataEl);
+  event.currentTarget.reset();
+};
+formEl.addEventListener("submit", onSubmitHandler);
