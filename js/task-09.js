d271df578e32352b84a0c8e544d5a5a244d1688d
MariaFandeeva <mfandeeva@gmail.com>
2022-09-15 18:30:36 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyEl = document.querySelector("body");
+const colorEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+
+const onClickChange = () => {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorEl.textContent = getRandomHexColor();
+};
+btnEl.addEventListener("click", onClickChange);
